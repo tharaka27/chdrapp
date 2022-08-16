0a0cd8f6d226a015353f742ef6eaccef0dc428ab
tharaka27 <tharakasachintha27@gmail.com>
2022-08-16 22:03:58 +0530

    login UI code refactoringT

--- app/src/main/java/com/echdr/android/echdrapp/service/Constants.java
+++ app/src/main/java/com/echdr/android/echdrapp/service/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static String APP_LANGUAGE_SINHALA = "si";
     public static String APP_LANGUAGE_ENGLISH= "en";
     public static String APP_LANGUAGE_TAMIL = "ta";
+    public static String ERHMIS_SERVER_URL = "https://www.erhmis.fhb.health.gov.lk/erhmis2356/api/";
 }
